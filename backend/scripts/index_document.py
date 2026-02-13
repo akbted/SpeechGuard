200dd017f9b13b8ad68677e1591c4712291d8a10
akbted <anantha.b@btech.christuniversity.in>
2026-02-14 00:47:37 +0530

    Refactor index_document.py to include logging setup and document indexing function

--- backend/scripts/index_document.py
+++ backend/scripts/index_document.py
@@ -1,2 +1,19 @@
 import os
 import glob
+import logging
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(name="Azure Document Indexer")
+
+def index_docs():
+    """
+    Read PDFs,
+    Chunk Documents,
+    Upload Document Azure
+    """
+
+    # Step 1 - Find the data folder path
+    

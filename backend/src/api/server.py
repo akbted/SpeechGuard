32779c6e7d9312465e431aba135dfc1f2a02e5b4
akbted <anantha.b@btech.christuniversity.in>
2026-02-15 17:03:40 +0530

    Add audit video endpoint with request and response models in server.py

--- backend/src/api/server.py
+++ backend/src/api/server.py
@@ -2,7 +2,7 @@ import uuid
 import logging
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 
 from backend.src.api.telemetry import setup_telemetry
 from backend.src.graph.workflow import app as compliance_graph
@@ -19,11 +19,67 @@ api = FastAPI(
 
 # Data Model
 class AuditRequest(BaseModel):
-    pass
+    video_url : str
 
 class ComplianceIssue(BaseModel):
-    pass
+    category: str
+    description: str
+    severity: str
+    time_stamp: Optional[str]
+    sub_category: Optional[str]
+    target_group: Optional[str]
+    flagged_text: Optional[str]
+    legal_reference: Optional[str]
 
 class AuditResponse(BaseModel):
-    pass
+    session_id: str         
+    video_id: str           
+    status: str              
+    final_report: str
+    compliance_results: List[ComplianceIssue]
+    errors: List = []       
+
+# Model Entry
+@api.post("/audit")
+def audit_video(request: AuditRequest):
+
+    video_url = request.video_url
+
+    # Generate a session_id
+    session_id = str(uuid.uuid4())
+    video_id = f"Vid{session_id[:8]}"
+
+    logger.info(f"Created a session {video_id} - video url {video_url}")
+    
+    # Graph Input
+    initial_state = {
+        "video_url": video_url,  
+        "video_id": video_id,
+        "compliance_results": [],
+        "errors": []
+    }
+
+    try:
+        final_state = compliance_graph.invoke(initial_state)
+        final_state_json = request.model_dump()
+
+        return AuditResponse(
+                session_id=str(session_id),
+                video_id=final_state.get("video_id", video_id),
+                
+                status=final_state.get("final_status", "UNKNOWN"),
+                final_report=final_state.get("final_report", "No report generated."),
+                compliance_results=final_state.get("compliance_results", []),
+                errors=final_state.get("errors", [])
+            )
+        
+    except Exception as e:
+        logger.error(f"Audit Failed: {str(e)}")  
+        
+        raise HTTPException(
+            status_code=500, 
+            detail=f"Workflow Execution Failed: {str(e)}"
+        )
+
+
 
